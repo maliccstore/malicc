07d4e14c6304a231344ff90f605fd06efba86e99
Shahzer Baig <shahzer.baig.sb@gmail.com>
2025-02-20 18:34:32 +0530

    feat(user model): Added new user model

---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public password!: string;
+  public email!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  { sequelize, modelName: "User", tableName: "users", timestamps: true }
+);
+
+export default User;
