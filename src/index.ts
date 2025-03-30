b151f7c6f73345b8ccbca9bb7a9ac47e450c0361
Shahzer Baig <shahzer.baig@gmail.com>
2025-03-30 22:38:40 +0530

    Modified(index): Routes are Added for User Routes.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,25 @@
-import express, { Express, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import requestSniffer from "./middlewares/requestsniffer";
-import userService from "./service/user.service";
+import UserRouter from "./api/User.route";
+
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use("/api", UserRouter);
 
-app.use(requestSniffer);
-
-function root(res: Response): void {
+function root(req: Request, res: Response): void {
   res.status(200).json({ message: "This is root path of malicc server" });
 }
 
 app.get("/", root);
 
-try {
-  app
-    .listen(port, () => {
-      console.log("Server running on port", port);
-    })
-    .on("error", (err) => {
-      console.error("Server error:", err);
-    });
-} catch (error) {
-  console.log(error);
-}
+app.listen(port, () => {
+  console.log("Server running on port", port);
+});
 
-userService();
+app.on("error", (error) => {
+  console.error("Server error:", error);
+  process.exit(1);
+});
