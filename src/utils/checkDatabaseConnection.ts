d6ae06cacd6462ff3356cf40d80ebe8e8fe1f0d6
Shahzer Baig <shahzer.baig@gmail.com>
2025-04-03 17:18:56 +0530

    Feat(checkDatabaseConnection): Wrote a new feature for checking the database connection.

---
+++ src/utils/checkDatabaseConnection.ts
@@ -0,0 +1,13 @@
+import sequelize from "../config/database";
+
+async function checkDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connection has been established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+    process.exit(1); // Exit process with failure
+  }
+}
+
+export default checkDatabaseConnection;
