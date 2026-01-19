98ad8373489ab7adf031a98c5220c79a7d171d92
Shahzer Baig <shahzer.baig@gmail.com>
2026-01-19 18:07:40 +0530

    Feat(order): Added Order table for the database

---
+++ src/models/Order.ts
@@ -0,0 +1,55 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  AllowNull,
+  Index,
+  Default,
+} from "sequelize-typescript";
+
+import { OrderStatus } from "../enums/OrderStatus";
+import { Currency } from "../enums/Currency";
+
+@Table({
+  tableName: "orders",
+  timestamps: true,
+})
+export class Order extends Model {
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: DataType.UUIDV4,
+  })
+  id!: string;
+
+  @AllowNull(true)
+  @Index
+  @Column(DataType.INTEGER)
+  userId?: number;
+
+  @AllowNull(true)
+  @Index
+  @Column(DataType.STRING)
+  sessionId?: string;
+
+  @Default(OrderStatus.CREATED)
+  @Column(DataType.ENUM(...Object.values(OrderStatus)))
+  status!: OrderStatus;
+
+  @Column(DataType.DECIMAL(10, 2))
+  subtotal!: number;
+
+  @Column(DataType.DECIMAL(10, 2))
+  tax!: number;
+
+  @Column(DataType.DECIMAL(10, 2))
+  shipping!: number;
+
+  @Column(DataType.DECIMAL(10, 2))
+  totalAmount!: number;
+
+  @Default(Currency.INR)
+  @Column(DataType.ENUM(...Object.values(Currency)))
+  currency!: Currency;
+}
