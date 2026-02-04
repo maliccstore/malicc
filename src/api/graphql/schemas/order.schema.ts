4a2059d4eb52f47871541ea5bebbce8038b6e933
Shah <41285362+shahzerbaig@users.noreply.github.com>
2026-02-04 19:06:32 +0530

    Fix(FullfilmentStatus)

--- src/api/graphql/schemas/order.schema.ts
+++ src/api/graphql/schemas/order.schema.ts
@@ -1,130 +1,143 @@
-import { ObjectType, Field, ID, Float, registerEnumType, Int } from "type-graphql";
+import {
+  ObjectType,
+  Field,
+  ID,
+  Float,
+  registerEnumType,
+  Int,
+} from "type-graphql";
 import { OrderStatus } from "../../../enums/OrderStatus";
 import { Currency } from "../../../enums/Currency";
 
+import { FulfillmentStatus } from "../../../enums/FulfillmentStatus";
+
 registerEnumType(OrderStatus, {
-    name: "OrderStatus",
-    description: "Status of the order",
+  name: "OrderStatus",
+  description: "Status of the order",
 });
 
 registerEnumType(Currency, {
-    name: "Currency",
-    description: "Currency used for the order",
+  name: "Currency",
+  description: "Currency used for the order",
 });
 
+registerEnumType(FulfillmentStatus, {
+  name: "FulfillmentStatus",
+  description: "Physical fulfillment lifecycle of an order",
+});
 @ObjectType()
 export class AddressSnapshotSchema {
-    @Field()
-    fullName!: string;
+  @Field()
+  fullName!: string;
 
-    @Field()
-    phoneNumber!: string;
+  @Field()
+  phoneNumber!: string;
 
-    @Field()
-    addressLine1!: string;
+  @Field()
+  addressLine1!: string;
 
-    @Field({ nullable: true })
-    addressLine2?: string;
+  @Field({ nullable: true })
+  addressLine2?: string;
 
-    @Field()
-    city!: string;
+  @Field()
+  city!: string;
 
-    @Field()
-    state!: string;
+  @Field()
+  state!: string;
 
-    @Field()
-    postalCode!: string;
+  @Field()
+  postalCode!: string;
 
-    @Field()
-    country!: string;
+  @Field()
+  country!: string;
 }
 
 @ObjectType()
 export class OrderItemSchema {
-    @Field(() => ID)
-    id!: string;
+  @Field(() => ID)
+  id!: string;
 
-    @Field()
-    productId!: string;
+  @Field()
+  productId!: string;
 
-    @Field()
-    productName!: string;
+  @Field()
+  productName!: string;
 
-    @Field(() => Float)
-    unitPrice!: number;
+  @Field(() => Float)
+  unitPrice!: number;
 
-    @Field()
-    quantity!: number;
+  @Field()
+  quantity!: number;
 
-    @Field(() => Float)
-    totalPrice!: number;
+  @Field(() => Float)
+  totalPrice!: number;
 }
 
 @ObjectType()
 export class OrderSchema {
-    @Field(() => ID)
-    id!: string;
+  @Field(() => ID)
+  id!: string;
 
-    @Field(() => OrderStatus)
-    status!: OrderStatus;
+  @Field(() => OrderStatus)
+  status!: OrderStatus;
 
-    @Field(() => Float)
-    subtotal!: number;
+  @Field(() => Float)
+  subtotal!: number;
 
-    @Field(() => Float)
-    tax!: number;
+  @Field(() => Float)
+  tax!: number;
 
-    @Field(() => Float)
-    shippingFee!: number;
+  @Field(() => Float)
+  shippingFee!: number;
 
-    @Field(() => Float)
-    totalAmount!: number;
+  @Field(() => Float)
+  totalAmount!: number;
 
-    @Field(() => Currency)
-    currency!: Currency;
+  @Field(() => Currency)
+  currency!: Currency;
 
-    @Field(() => [OrderItemSchema])
-    items!: OrderItemSchema[];
+  @Field(() => [OrderItemSchema])
+  items!: OrderItemSchema[];
 
-    @Field(() => AddressSnapshotSchema, { nullable: true })
-    shippingAddress?: AddressSnapshotSchema;
+  @Field(() => AddressSnapshotSchema, { nullable: true })
+  shippingAddress?: AddressSnapshotSchema;
 
-    @Field({ nullable: true })
-    paymentMethod?: string;
+  @Field({ nullable: true })
+  paymentMethod?: string;
 
-    @Field({ nullable: true })
-    shippingMethod?: string;
+  @Field({ nullable: true })
+  shippingMethod?: string;
 
-    @Field(() => Date)
-    createdAt!: Date;
+  @Field(() => Date)
+  createdAt!: Date;
 
-    @Field(() => Date)
-    updatedAt!: Date;
+  @Field(() => Date)
+  updatedAt!: Date;
 }
 
 @ObjectType()
 export class OrderResponse {
-    @Field()
-    success!: boolean;
+  @Field()
+  success!: boolean;
 
-    @Field({ nullable: true })
-    message?: string;
+  @Field({ nullable: true })
+  message?: string;
 
-    @Field(() => OrderSchema, { nullable: true })
-    order?: OrderSchema;
+  @Field(() => OrderSchema, { nullable: true })
+  order?: OrderSchema;
 }
 
 @ObjectType()
 export class OrdersResponse {
-    @Field()
-    success!: boolean;
+  @Field()
+  success!: boolean;
 
-    @Field({ nullable: true })
-    message?: string;
+  @Field({ nullable: true })
+  message?: string;
 
-    @Field(() => [OrderSchema])
-    orders!: OrderSchema[];
+  @Field(() => [OrderSchema])
+  orders!: OrderSchema[];
 
-    @Field(() => Int, { nullable: true })
-    totalCount?: number;
+  @Field(() => Int, { nullable: true })
+  totalCount?: number;
 }
